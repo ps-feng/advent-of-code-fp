9568164b7269216ec03537a078cf6550a2a9e5fc
Pin-Sho Feng <pin-sho.feng@xing.com>
2019-11-18 13:04:27 +0100

    Add part 2 from day 1

--- src/day1/Day1.purs
+++ src/day1/Day1.purs
@@ -1,25 +1,76 @@
-module Day1 where
+module Day1
+  ( day1Part1
+  , day1Part2
+  ) where
 
-import Data.Foldable (foldl)
+import Data.List (List(..), foldl, fromFoldable, (:))
+import Prelude (bind, map, pure, ($), (+), (<$>), (>=>))
 import Data.Maybe (Maybe(..))
 import Data.Number (fromString, nan)
-import Data.Traversable (sequence)
+import Data.Set as S
 import Data.String as String
+import Data.Traversable (sequence)
 import Effect (Effect)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync as FsSync
-import Prelude
 
-day1 :: Effect Number
-day1 = do
+day1Part1 :: Effect Number
+day1Part1 = do
   input <- readFileLines "test/day1/input.txt"
-  let parsed = fromString <$> input -- Array (Optional Number)
-  let floats = sequence parsed -- Optional (Array Number)
-  let result = foldl (+) 0.0 <$> floats
+  let
+    parsed :: Array (Maybe Number)
+    parsed = fromString <$> input
+  let
+    floats :: Maybe (Array Number)
+    floats = sequence parsed
+  let
+    result = foldl (+) 0.0 <$> floats
   case result of
     Just x -> pure x
     Nothing -> pure nan
-  
+
+day1Part2 :: Effect Number
+day1Part2 = do
+  input <- readFileLines "test/day1/input.txt"
+  let
+    parsed = fromString <$> input
+  let
+    floats = sequence parsed
+  let
+    maybeNumber = map day1Part2' floats
+  case maybeNumber of
+    Just x -> pure x
+    Nothing -> pure nan
+
+day1Part2' :: Array Number -> Number
+day1Part2' [] = nan
+
+day1Part2' deltas = part2' 0.0 (S.singleton 0.0) (fromFoldable deltas)
+  where
+  part2' :: Number -> S.Set Number -> List Number -> Number
+  part2' accum seen Nil = part2' accum seen (fromFoldable deltas)
+
+  part2' accum seen (x : xs) =
+    let
+      newAccum = accum + x
+    in
+      if newAccum `S.member` seen then
+        newAccum
+      else
+        part2' newAccum (S.insert newAccum seen) xs
+
+-- Lazy solution we could not get to work...
+-- part2 :: Array Number -> Number
+-- part2 deltas = part2' (S.singleton 0.0) (scanl (+) 0.0 $ cycle $ fromFoldable deltas)
+--   where
+--   part2' :: S.Set Number -> List Number -> Number 
+--   part2' seen ds = case step ds of
+--     Cons x xs ->
+--       if x `S.member` seen then
+--         trace (show x) (\_ -> x)
+--       else
+--         part2' (S.insert x seen) $ trace (show xs) (\_ -> xs)
+--     _ -> nan
 readFileLines :: String -> Effect (Array String)
 readFileLines filePath = do
   text <- FsSync.readTextFile UTF8 filePath
--- test/day1/Day1.purs
+++ test/day1/Day1.purs
@@ -1,12 +1,21 @@
 module Test.Day1 where
 
-import Prelude
-
-import Day1 (day1)
+import Day1 (day1Part1, day1Part2)
 import Effect (Effect)
+import Prelude (Unit, bind, discard)
 import Test.Assert (assertEqual)
 
 main :: Effect Unit
 main = do
-  number <- day1
+  testDay1Part1
+  testDay1Part2
+
+testDay1Part1 :: Effect Unit
+testDay1Part1 = do
+  number <- day1Part1
   assertEqual { actual: number, expected: 472.0 }
+
+testDay1Part2 :: Effect Unit
+testDay1Part2 = do
+  number <- day1Part2
+  assertEqual { actual: number, expected: 66932.0 }
