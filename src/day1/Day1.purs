7a478689a0871e59d8751a7183ee56ad579d2ca9
Pin-Sho Feng <pin-sho.feng@xing.com>
2019-11-18 10:41:12 +0100

    Add solution to day 1

--- src/day1/Day1.purs
+++ src/day1/Day1.purs
@@ -1,9 +1,33 @@
 module Day1 where
 
-import Prelude
+import Data.Foldable (foldl)
+import Data.Maybe (Maybe(..))
+import Data.Number (fromString, nan)
+import Data.Traversable (sequence)
+import Data.String as String
 import Effect (Effect)
-import Effect.Console (log)
+import Node.Encoding (Encoding(..))
+import Node.FS.Sync as FsSync
+import Prelude
 
-day1 :: Effect Unit
+day1 :: Effect Number
 day1 = do
-  log "Hello sailor!"
+  input <- readFileLines "test/day1/input.txt"
+  let parsed = fromString <$> input -- Array (Optional Number)
+  let floats = sequence parsed -- Optional (Array Number)
+  let result = foldl (+) 0.0 <$> floats
+  case result of
+    Just x -> pure x
+    Nothing -> pure nan
+  
+readFileLines :: String -> Effect (Array String)
+readFileLines filePath = do
+  text <- FsSync.readTextFile UTF8 filePath
+  let
+    lines = splitByNewline text
+  pure $ splitByNewline text
+
+splitByNewline ∷ String → Array String
+splitByNewline =
+  String.split (String.Pattern "\n")
+    >=> String.split (String.Pattern "\r")
--- test/day1/Day1.purs
+++ test/day1/Day1.purs
@@ -1,9 +1,12 @@
 module Test.Day1 where
 
 import Prelude
+
+import Day1 (day1)
 import Effect (Effect)
-import Test.Assert (assert)
+import Test.Assert (assertEqual)
 
 main :: Effect Unit
 main = do
-  assert (1 == 2)
+  number <- day1
+  assertEqual { actual: number, expected: 472.0 }
