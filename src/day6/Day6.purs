ef7439242e484d338575ef034b7e9630df320db4
Pin-Sho Feng <pin-sho.feng@xing.com>
2020-04-20 17:42:57 +0200

    WIP

--- src/day6/Day6.purs
+++ src/day6/Day6.purs
@@ -1,26 +1,85 @@
 module Day6
   ( day6Part1
   , day6Part2
+  , fillGrid
   , isInBoundingBox
-  , LocationInfo(..)
   , Coord(..)
+  , CoordState(..)
+  , Location(..)
   , makeCoord
-  , positionsAtDistance
-  , newPositionsAtDistance
+  , prettyPrint
   ) where
 
-import Data.Array ((..), length, slice, zipWith)
-import Data.Foldable (maximumBy)
+import Prelude
+
+import Effect.Console (log)
+
+import Data.Array ((..), drop, fold, insert, length, snoc, take, zipWith) as A
+import Data.Foldable (foldl, maximumBy)
 import Data.Function (on)
 import Data.Int (fromString)
-import Data.Map (Map)
+import Data.Map (Map, empty, insert, lookup, member, toUnfoldable, values)
 import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Set (Set, empty, fromFoldable, insert, member) as S
 import Data.String.Common (split)
 import Data.String.Pattern (Pattern(..))
 import Data.Traversable (traverse)
+import Data.Tuple (Tuple(..), fst, snd)
 import Effect (Effect)
-import Prelude
 import Util (readFileLines)
+import Debug.Trace (trace)
+
+-- Algorithm
+-- 1. Go through each location, one at a time:
+--    a. Flood +1 manhattan distance
+--    b. If we run into a visited square, update grid with new closest location
+--    c. Stop when we can't mark any square anymore
+-- 2. Loop through the table keeping
+--    - Set of infinite locations (letters whose x=0 or x=table.length or y = 0 or y=table.length)
+--    - Count occurrences of each of the locations that are not infinite
+-- 3. Stop when we can't continue marking for any of the locations
+-- 4. Find out max count
+
+type MaxAreaAcc =
+  { infiniteLocations :: S.Set Location
+  , nonInfiniteLocationToCount :: Map Location Int
+  }
+  
+type Coord
+  = { x :: Int
+    , y :: Int
+    }
+
+type Location
+  = Coord
+
+data CoordState
+  = ClosestTo Location Int
+  | ClosestToMultipleLocations Int
+
+derive instance eqCoordState :: Eq CoordState
+
+instance showCoordState :: Show CoordState where
+  show (ClosestTo location distance) = "ClosestTo " <> show location <> " " <> show distance
+  show (ClosestToMultipleLocations distance) = "ClosestToMultipleLocations " <> show distance
+
+type Grid
+  = Map Coord CoordState
+
+type GridUpdateResult
+  = Tuple Grid Int
+
+type Distance
+  = Int
+
+type BoundingBox
+  = 
+  { maxX :: Int
+  , maxY :: Int 
+  }
+
+type Solver
+  = (Array Location -> Int)
 
 day6Part1 :: String -> Effect (Maybe Int)
 day6Part1 filePath = solve filePath (\_ -> 1)
@@ -28,15 +87,13 @@ day6Part1 filePath = solve filePath (\_ -> 1)
 day6Part2 :: String -> Effect (Maybe Int)
 day6Part2 filePath = solve filePath (\_ -> 1)
 
-type Solver
-  = (Array Coord -> Int)
-
 solve :: String -> Solver -> Effect (Maybe Int)
 solve filePath solver = do
   input <- readFileLines filePath
-  pure $ do
-    locations <- traverse splitLineToCoord input
-    pure $ solver locations
+  pure
+    $ do
+        locations <- traverse splitLineToCoord input
+        pure $ solve' locations
 
 splitLineToCoord :: String -> Maybe Coord
 splitLineToCoord input = splitLineToCoord' $ split (Pattern ", ") input
@@ -45,96 +102,198 @@ splitLineToCoord input = splitLineToCoord' $ split (Pattern ", ") input
 
   splitLineToCoord' _ = Nothing
 
-type Coord
-  = 
-  { x :: Int
-  , y :: Int
-  }
+solve' :: Array Location -> Int
+solve' locations =
+  let
+    bbox = boundingBox locations
+  in
+    maxArea bbox $ fillGrid bbox locations
 
 makeCoord :: Int -> Int -> Coord
 makeCoord x y = { x: x, y: y }
-
-findLargestArea :: Array Coord -> Int
-findLargestArea locations = 10
-  where
-  { maxX: bboxX, maxY: bboxY } = boundingBox locations
-  isInBBox = isInBoundingBox bboxX bboxY
 
 isInBoundingBox :: Int -> Int -> Coord -> Boolean
 isInBoundingBox maxX maxY coord =
   0 <= coord.x && coord.x < maxX &&
   0 <= coord.y && coord.y < maxY
 
-boundingBox :: Array Coord -> { maxX :: Int, maxY :: Int}
-boundingBox locations =
-  { maxX: maxX, maxY: maxY}
+isAtBoundary :: Int -> Int -> Coord -> Boolean
+isAtBoundary maxX maxY coord =
+  coord.x == 0 || coord.x == maxX - 1 || coord.y == 0 || coord.y == maxY - 1
+
+boundingBox :: Array Coord -> BoundingBox
+boundingBox locations = { maxX: maxX, maxY: maxY }
   where
-  maxX = fromMaybe 0 $ (_+1) <$> (_.x <$> maximumBy (compare `on` _.x) locations)
-  maxY = fromMaybe 0 $ (_+1) <$> (_.y <$> maximumBy (compare `on` _.y) locations)
+  maxX = fromMaybe 0 $ (_ + 1) <$> (_.x <$> maximumBy (compare `on` _.x) locations)
 
-data LocationInfo 
-  = Unvisited
-  | ClosestTo Int String
-  | Equidistant Int
+  maxY = fromMaybe 0 $ (_ + 1) <$> (_.y <$> maximumBy (compare `on` _.y) locations)
 
-instance showLocationInfo :: Show LocationInfo where
-  show Unvisited = "Unvisited"
-  show (ClosestTo distance location) = "ClosestTo " <> show distance <> " location " <> show location
-  show (Equidistant distance) = "Equidistant " <> show distance
+-- Fill the grid
+-- let setOfInfiniteLocations
+-- let mapOfNonInfiniteLocationToCount
+-- 
+-- For each coord in grid do:
+--   if in setOfInfiniteLocations then continue
+--   else if coord.isAtBoundary then insert coord setOfInfiniteLocations
+--   else alter (+1) coord mapOfNonInfiniteLocationToCount
+-- 
+-- maxBy { k, v -> v} mapOfNonInfiniteLocationToCount
 
-type Grid = Map Coord LocationInfo
+maxArea :: BoundingBox -> Grid -> Int
+maxArea bbox grid =
+  let 
+    infiniteLocations = S.empty
+    nonInfiniteLocationToCount = empty
+    gridKeyValues = toUnfoldable grid :: Array (Tuple Coord CoordState)
+    { infiniteLocations: _, nonInfiniteLocationToCount } = 
+      foldl go { infiniteLocations: S.empty, nonInfiniteLocationToCount: empty } gridKeyValues
+        where
+          isAtBoundary' = isAtBoundary bbox.maxX bbox.maxY
 
-fillGrid :: Coord -> Int -> Grid -> Grid
-fillGrid coord distance grid =
-  grid
-
-newPositionsAtDistance :: Coord -> Int -> Array Coord
-newPositionsAtDistance coord 0 = [coord]
-newPositionsAtDistance { x: rx, y: ry } distance = 
-  let
-    -- Given distance 2 for position (0, 0)
-    -- top ys:    0 -1 -2 -1  0
-    -- xs:       -2 -1  0  1  2
-    -- bottom ys: 0  1  2  1  0
-    -- We s merge top ys and bottom ys for more efficiency
-    distanceMinusOne = (distance - 1)
-    negateDistance = negate distance
-    negateDistanceMinusOne = negate distanceMinusOne
-    xs = 0 .. distanceMinusOne <> distance .. negateDistance <> negateDistanceMinusOne .. (-1)
-    ys = negateDistance .. distanceMinusOne <> distance .. negateDistanceMinusOne
+          go :: MaxAreaAcc -> Tuple Coord CoordState -> MaxAreaAcc
+          go acc@{ infiniteLocations, nonInfiniteLocationToCount } (Tuple coord _) =
+            if S.member coord infiniteLocations then acc
+            else if isAtBoundary' coord then acc { infiniteLocations = S.insert coord infiniteLocations }
+            else
+              case lookup coord nonInfiniteLocationToCount of
+                Just count -> acc { nonInfiniteLocationToCount = insert coord (count + 1) nonInfiniteLocationToCount}
+                Nothing -> acc { nonInfiniteLocationToCount = insert coord 1 nonInfiniteLocationToCount}
   in
-    zipWith (\x y -> { x: x + rx, y: y + ry }) xs ys
+    foldl max 0 (values nonInfiniteLocationToCount)
 
--- Kept for benchmarking purposes
-positionsAtDistance :: Coord -> Int -> Array Coord
-positionsAtDistance location distance =
-  let
-    -- Given distance 2 for position (0, 0)
-    -- ysTop:    0 -1 -2 -1  0
-    -- xs:      -2 -1  0  1  2
-    -- ysBottom: 0  1  2  1  0
-    yRangeBottom = (0 .. distance) <> ((distance - 1) .. 0)
-    yRangeTop = map negate yRangeBottom
-    xRange = (-distance .. 0) <> (1 .. distance)
-    len = length yRangeBottom
-    -- first and last elements match with top and bottom, so we slice it
-    top = slice 1 (len-1) $ zipWith pairToCoord xRange yRangeTop
-    bottom = zipWith pairToCoord xRange yRangeBottom
-  in
-    top <> bottom
+chunked :: forall a. Array a -> Int -> Array (Array a)
+chunked array len =
+  chunked' array []
   where
-    pairToCoord a b = { x: location.x + a, y: location.y + b }
+    chunked' :: Array a -> Array (Array a) -> Array (Array a)
+    chunked' arr intermediateArray =
+      if A.length arr == 0 then intermediateArray
+      else
+        let
+          newElems = A.take len arr :: Array a
+          leftOver = A.drop len arr
+          newIntermediateArray = A.snoc intermediateArray newElems
+        in
+          chunked' leftOver intermediateArray 
 
--- bruteForcePositionAtDistance :: Int -> Array Coord
--- bruteForcePositionAtDistance distance = do
---   x <- (negate distance) .. distance
---   y <- (negate distance) .. distance
---   guard $ (abs x) + (abs y)  == distance
---   pure { x: x, y: y }
---   where 
---     abs :: Int -> Int
---     abs x = if x >= zero then x else negate x
+prettyPrint :: Array Location -> Effect Unit
+prettyPrint locations = 
+  do
+    let bbox = boundingBox locations
+    let grid = fillGrid bbox locations
+    let list = 
+          do
+            y <- 0 A... (bbox.maxY - 1)
+            x <- 0 A... (bbox.maxX - 1)
+            let v = lookup { x, y } grid
+            pure $ case v of
+              Just (ClosestTo location _) -> show location.x <> show location.y
+              Just (ClosestToMultipleLocations _) -> "."
+              Nothing -> "?"
+    let lala = trace "lala" \_ -> chunked list (bbox.maxY - 1) :: Array (Array String)
+    let lines = map (foldl (\acc str -> acc <> ", " <> str) "") lala :: Array (String)
+    let loggedLines = map log lines :: Array (Effect Unit)
+    A.fold loggedLines
 
--- bruteForceCoordinatesFromPositionAtDistance :: Coord -> Int -> Array Coord
--- bruteForceCoordinatesFromPositionAtDistance { x: px, y: py } distance =
---   map (\{ x, y } -> { x: px + x, y: py + y }) $ bruteForcePositionAtDistance distance
+-- prettyPrint' :: Array Location -> Effect Unit
+-- prettyPrint' locations = 
+--   do
+--     let bbox = boundingBox locations
+--     let grid = fillGrid bbox locations
+--     let list = 
+--           do
+--             y <- 0 .. (bbox.maxY - 1)
+--             let v = map (\x -> log $ show x
+--              -- lookup { x, y } grid
+--               -- log $ case lookup {x, y} grid of
+--               --   Just (ClosestTo location _) -> show location.x <> "_" <> show location.y
+--               --   Just (ClosestToMultipleLocations _) -> "."
+--               --   Nothing -> "?"
+--             ) 0 .. (bbox.maxX - 1)
+--             pure $ v
+--     fold list
+
+fillGrid :: BoundingBox -> Array Location -> Grid
+fillGrid bbox locations =
+  let
+    isInBoundingBox' = isInBoundingBox bbox.maxX bbox.maxY
+    locationSet = S.fromFoldable locations
+  in
+    fillGrid' locationSet 1 isInBoundingBox' empty
+
+fillGrid' :: S.Set Location -> Distance -> (Coord -> Boolean) -> Grid -> Grid
+fillGrid' locations distance isInBoundingBox' currentGrid
+  | locations == S.empty = currentGrid
+  | otherwise =
+      let
+        Tuple newGrid newLocations = fillGrid'' locations distance isInBoundingBox' currentGrid
+      in
+        fillGrid' newLocations (distance + 1) isInBoundingBox' newGrid
+
+fillGrid'' :: S.Set Location -> Distance -> (Coord -> Boolean) -> Grid -> Tuple Grid (S.Set Location)
+fillGrid'' locations distance isInBoundingBox' currentGrid =
+  foldl go (Tuple currentGrid S.empty) locations
+  where
+    go acc location =
+      let 
+        curUpdatableLocations = snd acc
+        (Tuple updatedGrid numberOfValidUpdates) = updateGridForCellsAtDistanceFromLocation isInBoundingBox' location distance (fst acc)
+      in case numberOfValidUpdates of
+        0 -> Tuple updatedGrid curUpdatableLocations
+        _ -> Tuple updatedGrid (S.insert location curUpdatableLocations)
+
+updateGridForCellsAtDistanceFromLocation :: (Coord -> Boolean)  -- The bounding box to calculate if coord is valid (inside)
+                                         -> Location            -- The location for which we are updating the grid
+                                         -> Distance            -- The distance from the location
+                                         -> Grid                -- The current grid
+                                         -> GridUpdateResult    -- A tuple that contains the new grid and the number of valid changes
+updateGridForCellsAtDistanceFromLocation isInBoundingBox' location distance grid =
+  foldl updateCoordStateAtCoord (Tuple grid 0) $ coordsAtDistanceFromCoord location distance
+  where
+    updateCoordStateAtCoord all@(Tuple theGrid numberOfValidUpdates) coord 
+      | not (isInBoundingBox' coord) = all
+      | otherwise =
+          let
+            currentState = lookup coord theGrid
+            newState = mergeCoordState (ClosestTo location distance) currentState
+            theGrid' = insert coord newState theGrid
+            numberOfValidUpdates' = 
+              case newState of
+                (ClosestTo l _) | l == location -> numberOfValidUpdates + 1
+                _ -> numberOfValidUpdates
+          in Tuple theGrid' numberOfValidUpdates'
+            
+mergeCoordState :: CoordState        -- The state
+                -> Maybe CoordState  -- The state to merge into (if any)
+                -> CoordState        -- The merged state
+mergeCoordState state Nothing = state
+mergeCoordState state (Just current) =
+  case [ state, current ] of
+    [ ClosestTo llocation ldistance,  ClosestTo rlocation rdistance ] -> 
+      if llocation == rlocation
+      then current
+      else mergeCoordState' ldistance rdistance
+    [ ClosestToMultipleLocations ldistance, ClosestTo _ rdistance ] -> mergeCoordState' ldistance rdistance
+    [ ClosestTo _ ldistance, ClosestToMultipleLocations rdistance ] -> mergeCoordState' ldistance rdistance
+    [ ClosestToMultipleLocations ldistance, ClosestToMultipleLocations rdistance ] -> mergeCoordState' ldistance rdistance
+    _ -> current -- TODO: check this
+  where
+    mergeCoordState' stateDistance currentDistance 
+      | stateDistance == currentDistance = ClosestToMultipleLocations currentDistance
+      | stateDistance < currentDistance  = state
+      | otherwise                        = current
+
+coordsAtDistanceFromCoord :: Coord -> Distance -> Array Coord
+coordsAtDistanceFromCoord coord 0 = [coord]
+coordsAtDistanceFromCoord { x: rx, y: ry } distance = 
+  coordsAtDistanceFromCoord'
+  where
+    coordsAtDistanceFromCoord' =
+      let
+        distanceMinusOne = (distance - 1)
+        negateDistance = negate distance
+        negateDistanceMinusOne = negate distanceMinusOne
+        xs = 0 A... distanceMinusOne <> distance A... negateDistance <> negateDistanceMinusOne A... (-1)
+        ys = negateDistance A... distanceMinusOne <> distance A... negateDistanceMinusOne
+      in
+        A.zipWith (\x y -> { x: x + rx, y: y + ry }) xs ys
--- test/day6/Day6.purs
+++ test/day6/Day6.purs
@@ -1,44 +1,21 @@
 module Test.Day6 where
 
-import Data.Array (length, sort)
 import Data.Maybe (fromMaybe)
-import Day6 (day6Part1, day6Part2, makeCoord, newPositionsAtDistance, positionsAtDistance)
+import Day6 (day6Part1)
 import Effect (Effect)
-import Effect.Console as Console
 import Prelude
 import Test.Assert (assertEqual)
 
 main :: Effect Unit
 main = do
-  testa
-  -- testDay6Part1a
-  -- testDay6Part1b
-  -- testDay6Part2
-
-testa :: Effect Unit
-testa = do
-  Console.time "Lelle"
-  let a = sort $ newPositionsAtDistance (makeCoord 10 10) 10000
-  Console.log $ show $ length a
-  Console.timeEnd "Lelle"
-  Console.time "Pin"
-  let b = sort $ positionsAtDistance (makeCoord 10 10) 10000
-  Console.log $ show $ length b
-  Console.timeEnd "Pin"
-  assertEqual { actual: a, expected: b }
+  testDay6Part1a
 
 testDay6Part1a :: Effect Unit
 testDay6Part1a = do
   number <- day6Part1 "test/day6/input-test.txt"
-  assertEqual { actual: fromMaybe 0 number, expected: 10 }
+  assertEqual { actual: fromMaybe 0 number, expected: 17 }
 
 testDay6Part1b :: Effect Unit
 testDay6Part1b = do
   number <- day6Part1 "test/day6/input.txt"
-  assertEqual { actual: fromMaybe 0 number, expected: 9060 }
-
-testDay6Part2 :: Effect Unit
-testDay6Part2 = do
-  
-  number <- day6Part2 "test/day6/input.txt"
-  assertEqual { actual: fromMaybe 0 number, expected: 6310 }
+  assertEqual { actual: fromMaybe 0 number, expected: 10 }
