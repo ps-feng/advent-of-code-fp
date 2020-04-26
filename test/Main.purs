22f082de683fbbf11e1068e57769a6b8c8ac2f13
Manuele Mion <manuele.mion@xing.com>
2020-04-26 08:03:00 +0200

    Day9: Added test for the provided "examples"

--- test/Main.purs
+++ test/Main.purs
@@ -3,7 +3,7 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
-import Test.Day1 as Day1
+-- import Test.Day1 as Day1
 -- import Test.Day2 as Day2
 -- import Test.Day3 as Day3
 -- import Test.Day4 as Day4
@@ -11,11 +11,11 @@ import Test.Day1 as Day1
 -- import Test.Day6 as Day6
 -- import Test.Day7 as Day7
 -- import Test.Day8 as Day8
--- import Test.Day9 as Day9
+import Test.Day9 as Day9
 
 main :: Effect Unit
 main = do
-  Day1.main 
+  -- Day1.main 
   -- Day2.main
   -- Day3.main
   -- Day4.main
@@ -23,4 +23,4 @@ main = do
   -- Day6.main
   -- Day7.main
   -- Day8.main
-  -- Day9.main
+  Day9.main
--- test/day9/Day9.purs
+++ test/day9/Day9.purs
@@ -2,13 +2,34 @@ module Test.Day9 where
 
 import Prelude
 
+import Data.Foldable (for_)
 import Day9 (day9Part1)
 import Effect (Effect)
 import Test.Assert (assertEqual)
 
 main :: Effect Unit
 main = do
-  testDay9Part1a
+  testDay9Part1Examples
+
+testDay9Part1Examples :: Effect Unit
+testDay9Part1Examples =
+  let examples = [ { numPlayers: 10, lastMarbleValue: 1618, expectedScore:   8317 }
+                 , { numPlayers: 13, lastMarbleValue: 7999, expectedScore: 146373 }
+                 , { numPlayers: 17, lastMarbleValue: 1104, expectedScore:   2764 }
+                 , { numPlayers: 21, lastMarbleValue: 6111, expectedScore:  54718 }
+                 , { numPlayers: 30, lastMarbleValue: 5807, expectedScore:  37305 }
+                 ]
+  in
+    for_ examples \{ numPlayers, lastMarbleValue, expectedScore } -> 
+      doTestDay9Part1 numPlayers lastMarbleValue expectedScore
+
+doTestDay9Part1 :: Int -- Number of players
+                -> Int -- Last marble value
+                -> Int -- Expected highest score
+                -> Effect Unit
+doTestDay9Part1 numPlayers lastMarbleValue expectedScore = do
+  actual <- day9Part1 numPlayers lastMarbleValue
+  assertEqual { actual: actual, expected: expectedScore }
 
 testDay9Part1a :: Effect Unit
 testDay9Part1a = do
